547490e9afa0c8cad97f4f2f7ebb34c16b280ca8
ErinUC <erinuc@gmail.com>
2016-05-10 12:01:11 -0400

    Installed Froala WYSIWYG editor gem

--- Gemfile
+++ Gemfile
@@ -13,11 +13,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'wysiwyg-rails'
 gem 'sanitize'
-
-
-
-# Use Capistrano for deployment
-# gem 'capistrano-rails', group: :development
+gem 'figaro', '~> 1.1', '>= 1.1.1'
 
   group :development, :test do
     gem 'better_errors'
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,29 +1,25 @@
 class PostsController < ApplicationController
+  include AmazonSignature
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-  # GET /posts
-  # GET /posts.json
   def index
     @posts = Post.all
   end
 
-  # GET /posts/1
-  # GET /posts/1.json
   def show
   end
 
-  # GET /posts/new
   def new
+    @hash = AmazonSignature::data_hash
     @post = Post.new
   end
 
-  # GET /posts/1/edit
   def edit
+    @hash = AmazonSignature::data_hash
   end
 
-  # POST /posts
-  # POST /posts.json
   def create
+    @hash = AmazonSignature::data_hash
     @post = Post.new(post_params)
 
     respond_to do |format|
