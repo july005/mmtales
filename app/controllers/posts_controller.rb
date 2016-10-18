196564f40170cacbd676b683c0ae2b6fc8742b09
ErinUC <erinuc@gmail.com>
2016-10-18 15:55:48 -0400

    Remove user authentication for search action in posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   include AmazonSignature
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :search]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
