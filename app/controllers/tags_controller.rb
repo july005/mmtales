3dbd6fa44db49b7f608724fdaa1174be35ee9fe9
ErinUC <erinuc@gmail.com>
2016-12-06 16:56:43 -0500

    HTML tags for tags views

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -6,6 +6,27 @@ class TagsController < ApplicationController
   def show
     @tag = ActsAsTaggableOn::Tag.find(params[:id])
     @posts = Post.tagged_with(@tag.name)
+    set_meta_tags title: @tag.name,
+                  site: 'monorails & mickey tales',
+                  reverse: true,
+                  description: 'The best unbiased Disney trip advice', 
+                  keywords: %w[Disney World Trip Planning Save Money Monorails Mickey Tales Travel Tips Hotels Parks Resorts Dining Walt],
+                  twitter: {
+                    card: "summary",
+                    site: "@monorails & mickey tales",
+                    description: 'The best unbiased Disney trip advice',
+                    image: "fairygirlhead.jpg"
+                  },
+                  og: {
+                    title:  "Monorails and Mickey Tales",
+                    description: @tag.name,
+                    type: 'website',
+            
+                    image: "fairygirlhead.jpg"
+                  },
+                  alternate: {
+                    href: 'http://www.monorailsandmickeytales.com/feed.rss', type: 'application/rss+xml', title: 'RSS'
+                  }
   end
 
 end
